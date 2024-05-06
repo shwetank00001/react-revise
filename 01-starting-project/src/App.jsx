f006dcb4d27a3f6db1ce893a30e5fae3ba9dccff
shwetank00001 <mshwetank@gmail.com>
2024-05-06 16:37:01 +0530

    ez

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -59,8 +59,19 @@ function App() {
           <main>
               <section id="core-concepts">
                   <h2>Time to get started!</h2>
-                  <CoreConcept image={CORE_CONCEPTS[0].image} title = {CORE_CONCEPTS[0].title} description= {CORE_CONCEPTS[0].description} />
+                  <CoreConcept
+                      image={CORE_CONCEPTS[0].image}
+                      title={CORE_CONCEPTS[0].title}
+                      description={CORE_CONCEPTS[0].description}
+                  />
+
+                  // we are using map here
                   {ele}
+
+
+                  WE LEARNED A NEW TRICK - IMPORTANT // A shortcut to use spread operator
+                  <CoreConcept {...CORE_CONCEPTS[0]} />
+                  <CoreConcept {...CORE_CONCEPTS[1]} />
               </section>
           </main>
       </div>
