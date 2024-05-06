553ae79238254cf838a8cd0842ef944cbf0e10bb
shwetank00001 <mshwetank@gmail.com>
2024-05-06 16:30:14 +0530

    gg

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -46,6 +46,11 @@ function CoreConcept(props){
   )
 }
 
+const ele = CORE_CONCEPTS.map(function(item){
+  return(
+    <CoreConcept image = {item.image} title = {item.title} description={item.description} />
+  )
+})
 
 function App() {
   return (
@@ -55,9 +60,7 @@ function App() {
               <section id="core-concepts">
                   <h2>Time to get started!</h2>
                   <CoreConcept image={CORE_CONCEPTS[0].image} title = {CORE_CONCEPTS[0].title} description= {CORE_CONCEPTS[0].description} />
-                  <CoreConcept />
-                  <CoreConcept />
-                  <CoreConcept />
+                  {ele}
               </section>
           </main>
       </div>
